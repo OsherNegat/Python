1c924b681270ff4d019ed6ae1929f6b6124e0ef3
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-20 16:04:30 +0200

    Delete EX-7.py

--- EX-7.py
+++
@@ -1,7 +0,0 @@
-Player_Name=input("Enter your Name: \n")
-Player_Age=int(input("Enter your Age: \n"))
-Player_Height=int(input("Enter your Height: \n"))  #>>> Height in cm
-if Player_Age<26 and Player_Age>21 and Player_Height>182:
-    print("Hi " + Player_Name + ", Welcome to Maccabi!")
-else:
-    print("Sorry, We aren't Accepting you! Good Luck :)")
