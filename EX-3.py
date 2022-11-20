c1d3123ddbc139b50250f93d4da2eec0d32c0cc4
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-20 16:04:11 +0200

    Delete EX-3.py

--- EX-3.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("Enter a number: \n"))
-num2=int(input("Enter a number: \n"))
-num3=int(input("Enter a number: \n"))
-if num1==1:
-    print(num2+num3)
-elif num1==2:
-    print(num3-num2)
-elif num1==3:
-    print(num1*3)
