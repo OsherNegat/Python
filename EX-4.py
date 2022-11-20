b8bd7f17cd4d5cdf684fff01e24aeca735ff3067
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-20 16:04:17 +0200

    Delete EX-4.py

--- EX-4.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter a number: \n"))
-b=int(input("Enter a number: \n"))
-x=int(input("Enter a number: \n"))
-if x>a and x<b or x<a and x>b:
-    print("In range")
-else:
-    print("Not in range")
