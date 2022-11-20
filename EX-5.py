11f7ba15ebc0acea73d06ff32f096c64051f60a7
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-20 16:04:22 +0200

    Delete EX-5.py

--- EX-5.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("Enter a number: \n"))
-num2=int(input("Enter a number: \n"))
-num3=int(input("Enter a number: \n"))
-if num1==num2==num3:
-    print("Equals")
-else:
-    print("Not Equals")
