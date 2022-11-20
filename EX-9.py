4579f94af51b5d6e2accd8b2c307c084c9c81064
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-20 16:04:40 +0200

    Delete EX-9.py

--- EX-9.py
+++
@@ -1,9 +0,0 @@
-Month_4 = int(input("Enter the baybie's mass in grams Before: \n"))
-This_Month = int(input("Enter the baybie's mass in grams Today: \n"))
-if This_Month > Month_4:  
-    Weight_Gained = This_Month - Month_4
-    print("Weight Gained: \n" + str(Weight_Gained))
-elif This_Month == Month_4:
-    print("The baby is in the same weight")
-else:
-    print("Weight Lost: \n" + str(Month_4-This_Month))
