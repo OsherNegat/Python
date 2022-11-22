8b6990a1ef45624659145a5a1871f66045b357a3
OsherNegat <118521510+OsherNegat@users.noreply.github.com>
2022-11-22 17:17:22 +0200

    Delete Math_Project.py

--- Math_Project.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter a number:  "))
-if num<=1000 or num>=10000:
-    print('Invalid Value...\nPlease Restart the program')
-    exit()
-Thousands=num//1000
-Hundreds=((num//100)%10)
-Tens=((num//10)%10)
-Ones=num%10
-print("Thousands = "+str(Thousands) +'\n'+"Hundreds = "+str(Hundreds)+'\n'+"Tens = "+str(Tens)+'\n'+"Ahadot = "+str(Ones))
