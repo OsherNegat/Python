fa65f76284f08221e634e9f7889875316c906056
Osher <oshernegat09@gmail.com>
2022-11-22 15:28:57 +0200

    Shift all Elements in list (Right or Left) Project

--- Exercises/Find_Numbers.py
+++ Exercises/Find_Numbers.py
@@ -1,10 +1,10 @@
 def value(string):
     list = []
     for i in range(len(string)):
-        if string[i].isdigit():
-            list.append(string[i])
+        if string[i].isdigit():  # Check if the string in index i is Numeric Value
+            list.append(string[i])  # Add the value to the list
     print("The numbers in the string are:  " + str(list))
-    return any(char.isdigit()for char in string)
+    return any(char.isdigit()for char in string)  # Check every char in the string, if it's a digit return it
 
 string = input("Enter a string:  ")
 print(value(string))
