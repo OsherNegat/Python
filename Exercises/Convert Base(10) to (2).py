74a4028352001cf8e82ed95cd5bec8c6d56001cd
Osher <oshernegat09@gmail.com>
2022-11-22 14:48:28 +0200

    Shift all Elements in list (Right or Left) Project

--- Exercises/Convert Base(10) to (2).py
+++ Exercises/Convert Base(10) to (2).py
@@ -1,9 +1,11 @@
-num = int(input("Enter a number:  "))
-for i in range(8):
-    if num % 2 == 0:
-        print('0')
+num = int(input("Enter a number:  "))  # Numeric Input
+result = []  # Create a list to store the values
+while num > 0:  # Whenever the number is above 0 enter the while loop
+    if num % 2 == 0:  # Check if the number has remainder
+        result.append('0') # Returns '0' because there's no remainder
     else:
-        print('1')
-    num = num / 2
-    if num < 1:
+        result.append('1') # Returns '1' because there's a remainder
+    num //= 2  # Returns the rounded value of the number and ignores the remainder
+    if num < 1:  # if the number is less than 1, than it's irrelevant
         break
+print("The number in Base 2 is:  " + str(result[::-1]))   # Prints the reversed list
