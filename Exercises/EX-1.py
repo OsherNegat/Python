bb4ca60c1549316b323664350797cbe97e773f76
Osher <oshernegat09@gmail.com>
2022-11-23 09:53:34 +0200

    My Exercises

--- Exercises/EX-1.py
+++ Exercises/EX-1.py
@@ -1,16 +1,18 @@
 def func(list):
     sum_digits = 0
-    num_digits = 0
-
-
+    for num in list:
+        length = len(str(num))
+        while num > 0:
+            sum_digits += (num % 10)  #
+            num //= 10
+        print("The length is: " + str(length) + "\nThe sum is:  " + str(sum_digits))
+        sum_digits = 0
+    return 0
 
 list = []
-i=0
-while i == 0:
-    num = int(input("Enter a number:  "))
-    if num < 0:
-        print("The number isn't POSITIVE...\nResuming the Program....")
-        break
+num = int(input("Enter a number:  "))
+while num >= 0:
     list.append(num)
+    num = int(input("Enter a number:  "))
 print(list)
-print(func(list))
+func(list)
